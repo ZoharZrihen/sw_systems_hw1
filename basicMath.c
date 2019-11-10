8d69b36f43a36c3929eb96b408f193060cf34915
r2rb <arthur.boltak@gmail.com>
2019-11-10 17:18:40 +0200

    upload homework

---
+++ basicMath.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "myMath.h"
+
+float add(float x,float y){
+	return (x+y);}
+float sub(float x, float y){
+	return add(x,-y);}
+double mul(double x, int y){
+	return (x*y);}
+double div(double x, int y){
+	if (y == 0)	{
+		printf("Cannot divide by 0\n");
+		return y;}
+	else return (x/y);}
+
---
+++ myMath.h
@@ -0,0 +1,11 @@
+
+#ifndef MYMATH_H
+#define MYMATH_H "myMath.h"
+float add(float,float);
+float sub(float,float);
+double mul(double,int);
+double div(double,int);
+double posPow(double,int);
+double Exp(int);
+double Pow(double,int);
+#endif
